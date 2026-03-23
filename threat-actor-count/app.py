59aef7b7eb8e690bf1a72eea85ec2bd7026a5aa9
David <dcapella@threatconnect.com>
2026-03-23 13:47:32 -0500

    feature: tcex init organization ingress

--- threat-actor-count/app.py
+++ threat-actor-count/app.py
@@ -11,4 +11,15 @@ class App(JobApp):
 
         This method should contain the core logic of the App.
         """
-        self.log.info(f'Sample Input is: {self.in_.dict()}')
+
+    def _get_groups(self):
+        """Get the groups from the ThreatConnect API."""
+        groups = self.tcex.api.tc.v3.groups()
+
+        # add one or more TQL filters
+        groups.filter.tql = 'hasThreatActorProfile()'
+
+        # iterate over results
+        for group in groups:
+            print(group.model.dict(exclude_none=True))
+            break
