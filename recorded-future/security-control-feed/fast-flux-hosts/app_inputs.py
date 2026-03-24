3fc905df2ec386d8e0ac999dc1d4fb2d0d965077
David <dcapella@threatconnect.com>
2026-03-24 13:28:20 -0500

    feature: initial setup
    `tcex init --type organization --template ingress`

---
+++ recorded-future/security-control-feed/fast-flux-hosts/app_inputs.py
@@ -0,0 +1,23 @@
+"""App Inputs"""
+
+from tcex.input.field_type import String
+from tcex.input.input import Input
+from tcex.input.model.app_organization_model import AppOrganizationModel
+
+
+class AppBaseModel(AppOrganizationModel):
+    """Base model for the App containing any common inputs."""
+
+    tc_owner: String
+
+
+class AppInputs:
+    """App Inputs"""
+
+    def __init__(self, inputs: Input):
+        """Initialize class properties."""
+        self.inputs = inputs
+
+    def update_inputs(self):
+        """Add custom App models to inputs. Validation will run at the same time."""
+        self.inputs.add_model(AppBaseModel)
